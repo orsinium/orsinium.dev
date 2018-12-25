87ecf3c2fe67fe5917203ac22d62ce2be22c9f1e
orsinium <master_fess@mail.ru>
2018-12-25 14:51:35 +0300

    +markdown support

--- generate.sh
+++ generate.sh
@@ -1,2 +1,3 @@
 mkdir public 2> /dev/null
-jinja2 -e loopcontrols index.html.j2 data.yml > public/index.html
+jinja2 -e loopcontrols -e jinja2_markdown.MarkdownExtension index.html.j2 data.yml > public/index.html
+
