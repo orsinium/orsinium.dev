8a8adba027189c6492cc5d82aa2c465f8f8033d4
gram <mail@orsinium.dev>
2022-04-11 08:42:12 +0200

    improve build script

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+echo "# install deps"
 python3 -m pip install -r requirements.txt
+echo "# update meta"
 python3 meta.py
+echo "# show missed"
 python3 missed.py
+echo "# generate HTML pages"
 python3 generate.py
