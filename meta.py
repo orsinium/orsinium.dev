147be53eaa8d012ddb852e3a4194590e5b03697d
gram <git@orsinium.dev>
2024-12-01 13:01:20 +0100

    add new go projects

--- meta.py
+++ meta.py
@@ -16,21 +16,27 @@ ORGS = [
 result = dict()
 for org in ORGS:
     print(org)
-    response = requests.get(
-        URL.format(org=org),
-        params=dict(per_page=100),
-        headers=HEADERS,
-    )
-    response.raise_for_status()
-    for project in response.json():
-        result[project['html_url']] = dict(
-            stars=project['stargazers_count'],
-            archived=project['archived'],
-            description=project['description'],
-            language=project['language'],
-            updated_at=project['pushed_at'].split('T')[0],
-            license=(project['license'] or {}).get('spdx_id', ''),
+    for page in range(1, 4):
+        if page > 1:
+            print(f'  page {page}')
+        response = requests.get(
+            URL.format(org=org),
+            params=dict(per_page=100, page=page),
+            headers=HEADERS,
         )
+        response.raise_for_status()
+        org_projects = response.json()
+        for project in org_projects:
+            result[project['html_url']] = dict(
+                stars=project['stargazers_count'],
+                archived=project['archived'],
+                description=project['description'],
+                language=project['language'],
+                updated_at=project['pushed_at'].split('T')[0],
+                license=(project['license'] or {}).get('spdx_id', ''),
+            )
+        if len(org_projects) < 100:
+            break
 
 with Path('data', 'meta.yml').open('w') as stream:
     yaml.dump(data=result, stream=stream)
